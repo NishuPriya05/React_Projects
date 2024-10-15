ad03897df5b4ce8d67dfa14364a70718b5d4a019
Nishu Priya <priyanishu0503@gmail.com>
2024-10-15 20:32:32 +0530

    context api small project

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
+import UserContextProvider from "./context/UserContextProvider"
+import './App.css'
+
+
+function App() {
+ 
+
+  return (
+    <UserContextProvider>
+      <h1>Context API</h1>
+     <Login />
+     <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
