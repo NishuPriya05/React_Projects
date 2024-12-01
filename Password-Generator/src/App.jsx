02dedd8e1d8f428935b62c9ff730d1700ccb980d
Nishu Priya <priyanishu0503@gmail.com>
2024-12-01 14:45:26 +0530

    projects

---
+++ Password-Generator/src/App.jsx
@@ -0,0 +1,104 @@
+import { useCallback, useEffect, useState, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const PasswordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numAllowed) {
+      str += "0123456789";
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*-_+=[]{}~`";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numAllowed, charAllowed, setPassword]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    PasswordGenerator();
+  }, [length, numAllowed, charAllowed, PasswordGenerator]);
+
+  return (
+    <div className="fifth">
+      <h1 className="sixth">Password Generator</h1>
+      <div className="ten">
+        <div className="d-flex">
+          <input
+            type="text"
+            value={password}
+            className="form-control my-input"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="my-button btn btn-primary"
+          >
+            copy
+          </button>
+        </div>
+
+        <div className="d-flex mt-3 home">
+          <div className="second">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="third">
+            <input
+              type="checkbox"
+              defaultChecked={numAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="fourth">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+        {/* <button type="button" className="btn btn-primary">Primary</button> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
