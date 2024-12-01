215c6bd5aa748b6e574ff0e8ca8ecb70f115d4af
Nishu Priya <priyanishu0503@gmail.com>
2024-12-01 14:50:13 +0530

    some changes

--- Password-Generator/src/App.jsx
+++ Password-Generator/src/App.jsx
@@ -95,7 +95,6 @@ function App() {
             <label htmlFor="characterInput">Characters</label>
           </div>
         </div>
-        {/* <button type="button" className="btn btn-primary">Primary</button> */}
       </div>
     </div>
   );
