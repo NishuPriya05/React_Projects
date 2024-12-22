5620d3c5f5aafdd4fff6345aa46410146f72de76
Nishu Priya <priyanishu0503@gmail.com>
2024-12-22 20:17:16 +0530

    react hooks form in production

--- MegaBlog/src/Components/Header/Header.jsx
+++ MegaBlog/src/Components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import { Container, Logo, LogoutBtn } from "../index";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectUser } from "../../store/authSlice";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
