2cabfddbc065c6d736b939c9f5f63f6060c75389
Nishu Priya <priyanishu0503@gmail.com>
2024-12-26 19:34:15 +0530

    mega blog

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
 import { Header, Footer } from "./Components/index.js";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -24,7 +25,7 @@ function App() {
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
         <Header />
-        <main>{/* <Outlet/> */}</main>
+        <main><Outlet/> </main>
         <Footer />
       </div>
     </div>
